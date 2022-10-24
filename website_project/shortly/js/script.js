4f5e3738791d82db39e2ad6eeaab2d5d717a1ae9
colbery <92644519+colbery@users.noreply.github.com>
2022-10-24 18:33:35 +0200

    end

--- website_project/shortly/js/script.js
+++ website_project/shortly/js/script.js
@@ -1,9 +1,17 @@
 const input = document.getElementById("link-input");
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
 const linkForm = document.getElementById("link-form");
 const errMsg = document.getElementById("err-msg");
 
 linkForm.addEventListener("submit", formSubmit);
+btn.addEventListener("click", navToggle);
 
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+}
 // Validate a URL
 function validURL(str) {
   var pattern = new RegExp(
