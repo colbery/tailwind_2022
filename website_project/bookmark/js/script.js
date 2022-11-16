70ec297d9e84d40c1a2380ff014c913adf292b93
colbery <92644519+colbery@users.noreply.github.com>
2022-11-16 18:23:01 +0100

    end_woho

--- website_project/bookmark/js/script.js
+++ website_project/bookmark/js/script.js
@@ -1,8 +1,14 @@
 const tabs = document.querySelectorAll(".tab");
 const panels = document.querySelectorAll(".panel");
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const logo = document.getElementById("logo");
 
 //tabs nasluchiwanie
 tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
+
+//ham button listener
+btn.addEventListener("click", navToggle);
 
 function onTabClick(e) {
   //wylacz bordery
@@ -26,4 +32,16 @@ function onTabClick(e) {
     .getElementById("panels")
     .getElementsByClassName(classString)[0]
     .classList.remove("hidden");
+}
+
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+
+  if (menu.classList.contains("flex")) {
+    logo.setAttribute("src", "./images/logo-bookmark-footer.svg");
+  } else {
+    logo.setAttribute("src", "./images/logo-bookmark.svg");
+  }
 }
